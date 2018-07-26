f837347a73d2a01c0b2703e3e93853a6646da65c
sooryagangarajk <sooryagangaraj@gmail.com>
2018-07-26 07:05:28 +0530

    axios fixed

--- React/src/home/home.js
+++ React/src/home/home.js
@@ -5,15 +5,55 @@ class Home extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <div className="jumbotron text-center">
-          <h1>Certificate Generation Home</h1>
+      <div className="home">
+        <div className=" jumbotron text-center shadow bg-dark text-white header">
+          <div>Certificate Generation Home</div>
         </div>
 
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6 bg-primary">hi</div>
-            <div className="col-sm-6 bg-info">heloo</div>
+        <div className=" container row">
+          <div className=" card col-6">
+            <img
+              src={"https://picsum.photos/200/300/?random"}
+              alt="John"
+              className=" myimg"
+            />
+            <h1>John Doe</h1>
+            <p className=" titl">my name here</p>
+          </div>
+          <div className="  col-6">
+            <div className="  row">
+              <div className=" card">
+                <h1>John Doe</h1>
+                <p className=" titl">IMAGE PROCESSING AND MACHINE LEARNING</p>
+                <p>Harvard University</p>
+                <div className=" row">
+                  <div className=" col-6">
+                    <button className=" btn-primary rounded">Print</button>
+                  </div>
+                  <div className=" col-6">
+                    <button className=" btn-success rounded">Generate</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="home  row">
+              <div className="home card">
+                <h1>John Doe</h1>
+                <p className="home titl">FULL STACK WEB DEVELOPMENT</p>
+                <p>Harvard University</p>
+                <div className="home row">
+                  <div className="home col-6">
+                    <button className="home btn-primary rounded">Print</button>
+                  </div>
+                  <div className="home col-6">
+                    <button className="home btn-success rounded">
+                      Generate
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
